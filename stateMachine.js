90e2314e1ff37126fafa38122bb425b3cf640b30
Susanna Huhtanen <susanna.huhtanen@iki.fi>
2020-01-10 18:53:07 +0100

    not under way

--- stateMachine.js
+++ stateMachine.js
@@ -1,7 +1,22 @@
 const where = require("where");
 
-let harbourTime = null;
-let harbourPosition = null;
+// We assume that the vessel is in harbour when system boots up
+let stateChangeTime = null;
+let stateChangePosition = null;
+let lastState = null;
+
+const notUnderWay = "not-under-way";
+
+const setState = (state, update) => {
+  if (state !== lastState){
+      stateChangeTime = update.time;
+      if (update.path === 'navigation.position'){
+        stateChangePosition = update.value;
+      }
+      lastState = state;
+  }
+  return state;
+} 
 
 module.exports = update => {
   //gets input update object {
@@ -10,7 +25,6 @@ module.exports = update => {
   //time: date`
   //}
 
-  const currentTime = new Date();
   //anchor postion has a value, we have dropped the anchor
   if (update.path === "navigation.anchor.position" && update.value) {
     return "anchoring";
@@ -19,10 +33,15 @@ module.exports = update => {
   if (update.path === "navigation.position") {
     //inHarbour we have moved less than 100 meters in 10 minutes
     // check that 10 minutes has passed
-    if (harbourTime.getTime() - currentTime.getTime() > 60000 * 10) {
+    if (! stateChangeTime ){
+      return setState(notUnderWay, update)
+    }
+
+    console.log(stateChangeTime, update.time, (stateChangeTime.getTime() - update.time.getTime()) / 1000);
+    if (stateChangeTime.getTime() - update.time.getTime() >= 60000 * 10) {
       //check that current position is less than 100 meters from the previous position
-      if (harbourPosition.distanceTo(update.value) <= 100) {
-        return "inHarbour";
+      if (!stateChangePosition || stateChangePosition.distanceTo(update.value) <= 100) {
+        return setState(notUnderWay, update);
       } else {
         //if we are not in harbour and engine is on, we are running with engine
         if (update.path === "navigation.engine" && update.value) {
--- test/stateMachine.test.js
+++ test/stateMachine.test.js
@@ -1,15 +1,17 @@
 var assert = require('assert');
 var stateMachine = require('../stateMachine');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
 
-describe('InHarbour', function() {
-    it('should return that we are in harbour', function(){
-          assert.equal('inHarbour', stateMachine())
+describe('not-under-way', function() {
+    it('should return that we are not under way', function(){
+          const update = {
+              path: "navigation.position",
+              value: {
+                  latitude: 60.254558,
+                  longitude: 25.042828
+              },
+              time: new Date()
+          };
+          update.time.setMinutes(update.time.getMinutes() - 11);
+          assert.equal('not-under-way', stateMachine(update))
         });
     });
