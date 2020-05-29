7a9c3fc4c9f3ab77a0314514e284fd71b189eaf5
Susanna Huhtanen <susanna.huhtanen@iki.fi>
2020-05-29 18:01:09 +0200

    Release 0.1.2

--- test/moored.test.js
+++ test/moored.test.js
@@ -1,18 +1,18 @@
 const StateMachine = require('../StateMachine');
 const stateUpdate = require('./utils/stateUpdate');
 
-describe('not-under-way', () => {
+describe('moored', () => {
   const stateMachine = new StateMachine();
   after(() => {
     stateUpdate.reset();
   });
   it('should return that we are not under way, when the system boots', () => {
-    stateUpdate.position(stateMachine, 'not-under-way', 60.254558, 25.042828, 0);
+    stateUpdate.position(stateMachine, 'moored', 60.254558, 25.042828, 0);
   });
   it('should return that we are not under way, when 10 minutes has not elapsed', () => {
-    stateUpdate.position(stateMachine, 'not-under-way', 60.254558, 25.042828, 5);
+    stateUpdate.position(stateMachine, 'moored', 60.254558, 25.042828, 5);
   });
   it('should return that we are not under way, when system is on and position is not changing', () => {
-    stateUpdate.position(stateMachine, 'not-under-way', 60.254558, 25.042828, 11);
+    stateUpdate.position(stateMachine, 'moored', 60.254558, 25.042828, 11);
   });
 });
